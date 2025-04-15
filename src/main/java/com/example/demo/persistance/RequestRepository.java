a19aa6b46567296ad321a03c729949099c592a71
pborissow <email@pborissow.dev>
2025-04-15 07:30:34 -0400

    Added new search interface to the RequestRepository

--- src/main/java/com/example/demo/persistance/RequestRepository.java
+++ src/main/java/com/example/demo/persistance/RequestRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.persistance;
 
+import org.springframework.data.domain.*;
 import org.springframework.data.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,7 @@ import org.springframework.stereotype.Repository;
 public interface RequestRepository extends PagingAndSortingRepository<RequestEntity, Long> {
 
     public RequestEntity save(RequestEntity entity);
+
+    public Page<RequestEntity> findAll(Pageable pageable);
 
 }
