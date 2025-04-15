a935dbe1b79131535fe07bdb4f7dfca97d26b0f7
pborissow <email@pborissow.dev>
2025-04-15 06:36:45 -0400

    Added new JSONMapper utility class

---
+++ src/main/java/com/example/demo/utils/JSONMapper.java
@@ -0,0 +1,67 @@
+package com.example.demo.utils;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import java.util.*;
+import java.text.*;
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class JSONMapper extends ObjectMapper {
+
+    private static SimpleModule s;
+    static {
+        s = new SimpleModule();
+        s.addSerializer(OffsetDateTime.class, new JsonSerializer<OffsetDateTime>(){
+
+            public void serialize(OffsetDateTime value, JsonGenerator jgen, SerializerProvider provider)
+            throws IOException {
+                String stringValue = value.toString();
+                if(stringValue != null && !stringValue.isEmpty() && !stringValue.equals("null")) {
+                    jgen.writeString(value.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
+                }
+                else {
+                    jgen.writeNull();
+                }
+            }
+
+            public Class<OffsetDateTime> handledType() {
+                return OffsetDateTime.class;
+            }
+        });
+
+        s.addSerializer(Date.class, new JsonSerializer<Date>(){
+            private final TimeZone tz = TimeZone.getTimeZone("UTC");
+
+            public void serialize(Date value, JsonGenerator jgen, SerializerProvider provider)
+            throws IOException {
+                String stringValue = value.toString();
+                if(stringValue != null && !stringValue.isEmpty() && !stringValue.equals("null")) {
+                    DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'"); // Quoted "Z" to indicate UTC, no timezone offset
+                    df.setTimeZone(tz);
+                    jgen.writeString(df.format(value));
+                }
+                else {
+                    jgen.writeNull();
+                }
+            }
+
+            public Class<Date> handledType() {
+                return Date.class;
+            }
+        });
+
+
+    }
+
+    public JSONMapper(){
+        super();
+        super.registerModule(s);
+        super.setSerializationInclusion(Include.NON_NULL);
+    }
+}
