5b92425d94fec4c0b99dc12b0aac0f5cdd6c79cc
pborissow <email@pborissow.dev>
2025-04-15 07:31:07 -0400

    Added new getRequests() method to the WebServices class

--- src/main/java/com/example/demo/WebServices.java
+++ src/main/java/com/example/demo/WebServices.java
@@ -1,19 +1,39 @@
 package com.example.demo;
+import static com.example.demo.utils.HttpUtils.*;
+import com.example.demo.persistance.RequestEntity;
+import com.example.demo.persistance.RequestRepository;
 
 import java.util.*;
-import org.springframework.web.bind.annotation.*;
 
+
+import org.springframework.http.*;
+import org.springframework.data.domain.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static javaxt.utils.Console.console;
 
 
 @RestController
 public class WebServices {
 
+    @Autowired
+    RequestRepository requestRepository;
+
+
+
+  //**************************************************************************
+  //** getDate
+  //**************************************************************************
     @RequestMapping(value = "/date/**", method = RequestMethod.GET)
     public String getDate(){
         return new java.util.Date().toString();
     }
 
 
+  //**************************************************************************
+  //** hello
+  //**************************************************************************
     @RequestMapping("/hello")
     public String hello(@RequestParam Map<String, String> params){
 
@@ -23,6 +43,23 @@ public class WebServices {
         }
 
         return str.toString();
+    }
+
+
+  //**************************************************************************
+  //** getRequests
+  //**************************************************************************
+    @RequestMapping("/requests")
+    @ResponseBody
+    public ResponseEntity getRequests(@RequestParam Map<String, String> params){
+
+        try{
+            Page<RequestEntity> page = requestRepository.findAll(getPageRequest(params));
+            return getResponse(page);
+        }
+        catch(Exception ex){
+            return getResponse(ex);
+        }
     }
 
 }
